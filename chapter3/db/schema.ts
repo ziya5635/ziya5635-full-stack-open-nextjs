a9b4100563991a8cf309f37977ef85ba71d307e7
reza5635 <reza5635@ymail.com>
2026-09-12 13:34:48 +0330

    uncomment drizzle configs

--- chapter3/db/schema.ts
+++ chapter3/db/schema.ts
@@ -1,10 +1,10 @@
-import { pgTable, serial, text, integer } from "drizzle-orm/pg-core"
+// import { pgTable, serial, text, integer } from "drizzle-orm/pg-core"
 
-export const blogs = pgTable("blogs", {
-    id: serial("id").primaryKey(),
-    author: text("author").notNull(),
-    title: text("title").notNull(),
-    url: text("url").notNull(),
-    likes: integer("likes").default(0),
-})
+// export const blogs = pgTable("blogs", {
+//     id: serial("id").primaryKey(),
+//     author: text("author").notNull(),
+//     title: text("title").notNull(),
+//     url: text("url").notNull(),
+//     likes: integer("likes").default(0),
+// })
 
